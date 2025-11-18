e240542a8b3d6f50efaf61802a440e365cc09a58
vaibhavuttam8 <vaibhavuttam8@users.noreply.github.com>
2025-11-18 18:25:23 +0530

    add : main page and quiz component page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { apiData } from './api'
+import Start from './components/Start'
 
 function App() {
   
 const [quizData, setQuizData] = useState(null)
+const [startQuiz, setStartQuiz] = useState(false)
 
 useEffect(()=>{
   const fetchData = async() => {
@@ -16,15 +16,30 @@ useEffect(()=>{
   fetchData()
 }, [])
 
+function handleStartQuiz(){
+  setStartQuiz(true)
+}
+
+const quizQuestions = quizData?.map((question, index) => {
+  return (<div key={index}>
+    <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
+    <div>
+      <button dangerouslySetInnerHTML={{ __html: question.correct_answer }}></button>
+      <button dangerouslySetInnerHTML={{ __html: question.incorrect_answers[0] }}></button>
+      <button dangerouslySetInnerHTML={{ __html: question.incorrect_answers[1] }}></button>
+      <button dangerouslySetInnerHTML={{ __html: question.incorrect_answers[2] }}></button>
+    </div>
+  </div>)
+})
+
 console.log(quizData)
 
   return (
     <>
-      <main>
-        <h1>Quizzical</h1>
-        <p>first solo project</p>
-        <button>Start Quiz</button>
-      </main>
+
+      { !startQuiz &&     <Start handleStartQuiz={handleStartQuiz} /> }
+      { startQuiz && quizQuestions }
+   
     </>
   )
 }
---
+++ src/components/Start.jsx
@@ -0,0 +1,9 @@
+export default function Start({handleStartQuiz}){
+    return (
+        <main>
+            <h1>Quizzical</h1>
+            <p>first solo project</p>
+            <button onClick={handleStartQuiz}>Start Quiz</button>
+        </main>
+    )
+}
