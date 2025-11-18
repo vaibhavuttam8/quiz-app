aa49f2d475b6f676efc2d7a01117c0c9e6faf74c
vaibhavuttam8 <vaibhavuttam8@users.noreply.github.com>
2025-11-18 20:24:50 +0530

    style component to center

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,42 +1,55 @@
+import { useState } from 'react'
+
 export default function Questions(props) {
+    const [selectedAnswers, setSelectedAnswers] = useState({})
 
-const quizQuestions = props.quizData?.map((question, index) => {
+    function handleAnswerClick(questionIndex, answer) {
+        setSelectedAnswers(prev => ({
+            ...prev,
+            [questionIndex]: answer
+        }))
+    }
 
-    
+    const quizQuestions = props.quizData?.map((question, index) => {
+        let answers = []
 
-   if(question.type === 'multiple') {
+        if(question.type === 'multiple') {
+            answers = [
+                question.correct_answer,
+                ...question.incorrect_answers
+            ].sort(() => Math.random() - 0.5) // Shuffle answers
+        }
+
+        if(question.type === 'boolean'){
+            answers = [
+                question.correct_answer,
+                ...question.incorrect_answers
+            ].sort(() => Math.random() - 0.5) // Shuffle answers
+        }
+
         return (
-        <div key={index}>
-            <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
-            <div>
-                <button dangerouslySetInnerHTML={{ __html: question.correct_answer }}></button>
-                <button dangerouslySetInnerHTML={{ __html: question.incorrect_answers[0] }}></button>
-                <button dangerouslySetInnerHTML={{ __html: question.incorrect_answers[1] }}></button>
-                <button dangerouslySetInnerHTML={{ __html: question.incorrect_answers[2] }}></button>
+            <div key={index} className="question-card">
+                <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
+                <div className="answers-grid">
+                    {answers.map((answer, answerIndex) => (
+                        <button 
+                            key={answerIndex}
+                            className={`answer-button ${selectedAnswers[index] === answer ? 'selected' : ''}`}
+                            onClick={() => handleAnswerClick(index, answer)}
+                            dangerouslySetInnerHTML={{ __html: answer }}
+                        />
+                    ))}
+                </div>
             </div>
-        </div>)
-   }
-
-   if(question.type === 'boolean'){
-        return(
-        <div key={index}>
-            <h2 dangerouslySetInnerHTML={{__html: question.question}}></h2>
-            <div>
-                <button dangerouslySetInnerHTML={{__html: question.incorrect_answers[0]}}></button>
-                <button dangerouslySetInnerHTML={{__html: question.correct_answer}}></button>
-            </div>
-        </div>
         )
-   }
-  
     })
-
 
     return(
         <main>
-        <section className="questions">
-        {quizQuestions}
-        </section>
+            <section className="questions-container">
+                {quizQuestions}
+                <button className="submit-button">Check Answers</button>
+            </section>
         </main>
     )
 }
